cefb2fcf932b9af16bfbf98e3f820e57cb0de867
wangfeng <562434023@qq.com>
2017-04-12 16:20:45 +0800

    first java

---
+++ Java_stu/src/com/wang/java_stu/hello/Hello.java
@@ -0,0 +1,13 @@
+package com.wang.java_stu.hello;
+
+import lombok.Setter;
+
+@Setter
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
